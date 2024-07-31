b37ef9d8295e28a5ca9d530a3d6ef649365aa075
Felipe Brito <139879477+FelipeBritoSP10@users.noreply.github.com>
2024-07-31 15:37:03 -0300

    Update moviment.js

--- moviment.js
+++ moviment.js
@@ -1,32 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const mobileMenuButton = document.getElementById('mobile-menu-button');
-  const mobileMenu = document.getElementById('mobile-menu');
-  const menuOpenIcon = document.getElementById('menu-open-icon');
-  const menuCloseIcon = document.getElementById('menu-close-icon');
-  
-  mobileMenuButton.addEventListener('click', function() {
-      const isOpen = mobileMenu.classList.contains('hidden');
-      
-      if (isOpen) {
-          mobileMenu.classList.remove('hidden');
-          menuOpenIcon.classList.add('hidden');
-          menuCloseIcon.classList.remove('hidden');
-      } else {
-          mobileMenu.classList.add('hidden');
-          menuOpenIcon.classList.remove('hidden');
-          menuCloseIcon.classList.add('hidden');
-      }
-  });
-});
-
-
-  document.querySelectorAll('.accordion-button').forEach(button => {
-    button.addEventListener('click', () => {
-        const content = button.nextElementSibling;
-        content.style.display = content.style.display === 'block' ? 'none' : 'block';
-    });
-});
-
 
 
 function openModal(modalId) {
@@ -37,3 +8,25 @@ function closeModal(modalId) {
   document.getElementById(modalId).classList.add('hidden');
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+  const menuButton = document.getElementById('menu-button');
+  const menu = document.getElementById('mobile-menu');
+  const menuOpenIcon = document.getElementById('menu-open');
+  const menuCloseIcon = document.getElementById('menu-close');
+
+  menuButton.addEventListener('click', function () {
+    const isMenuOpen = menu.classList.contains('block');
+    
+    if (isMenuOpen) {
+      menu.classList.add('hidden');
+      menu.classList.remove('block');
+      menuOpenIcon.classList.remove('hidden');
+      menuCloseIcon.classList.add('hidden');
+    } else {
+      menu.classList.add('block');
+      menu.classList.remove('hidden');
+      menuOpenIcon.classList.add('hidden');
+      menuCloseIcon.classList.remove('hidden');
+    }
+  });
+});
