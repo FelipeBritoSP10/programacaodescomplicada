de9adc7f2d373f9f120311d70e970d2ab9c29840
Felipe Brito <139879477+FelipeBritoSP10@users.noreply.github.com>
2024-07-31 13:31:27 -0300

    Update moviment.js

--- moviment.js
+++ moviment.js
@@ -1,26 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const mobileMenuButton = document.getElementById('mobile-menu-button');
-    const mobileMenu = document.getElementById('mobile-menu');
-    const menuOpenIcon = document.getElementById('menu-open-icon');
-    const menuCloseIcon = document.getElementById('menu-close-icon');
-
-    // Ensure the mobile menu is closed by default
-    mobileMenu.classList.add('hidden');
-    menuOpenIcon.classList.remove('hidden');
-    menuCloseIcon.classList.add('hidden');
-
-    mobileMenuButton.addEventListener('click', () => {
-      if (mobileMenu.classList.contains('hidden')) {
-        mobileMenu.classList.remove('hidden');
-        menuOpenIcon.classList.add('hidden');
-        menuCloseIcon.classList.remove('hidden');
+document.addEventListener('DOMContentLoaded', function() {
+  const mobileMenuButton = document.getElementById('mobile-menu-button');
+  const mobileMenu = document.getElementById('mobile-menu');
+  const menuOpenIcon = document.getElementById('menu-open-icon');
+  const menuCloseIcon = document.getElementById('menu-close-icon');
+  
+  mobileMenuButton.addEventListener('click', function() {
+      const isOpen = mobileMenu.classList.contains('hidden');
+      
+      if (isOpen) {
+          mobileMenu.classList.remove('hidden');
+          menuOpenIcon.classList.add('hidden');
+          menuCloseIcon.classList.remove('hidden');
       } else {
-        mobileMenu.classList.add('hidden');
-        menuOpenIcon.classList.remove('hidden');
-        menuCloseIcon.classList.add('hidden');
+          mobileMenu.classList.add('hidden');
+          menuOpenIcon.classList.remove('hidden');
+          menuCloseIcon.classList.add('hidden');
       }
-    });
   });
+});
 
 
   document.querySelectorAll('.accordion-button').forEach(button => {
@@ -29,3 +26,14 @@ document.addEventListener('DOMContentLoaded', () => {
         content.style.display = content.style.display === 'block' ? 'none' : 'block';
     });
 });
+
+
+
+function openModal(modalId) {
+  document.getElementById(modalId).classList.remove('hidden');
+}
+
+function closeModal(modalId) {
+  document.getElementById(modalId).classList.add('hidden');
+}
+
