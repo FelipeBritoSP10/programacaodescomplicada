fa5b8dc7951a04a85b640a84e74e3db78d29c799
Felipe Brito <139879477+FelipeBritoSP10@users.noreply.github.com>
2024-08-01 11:01:08 -0300

    Update moviment.js

--- moviment.js
+++ moviment.js
@@ -13,20 +13,25 @@ document.addEventListener('DOMContentLoaded', function () {
   const menu = document.getElementById('mobile-menu');
   const menuOpenIcon = document.getElementById('menu-open');
   const menuCloseIcon = document.getElementById('menu-close');
+  const menuCloseMobile = document.getElementById('menu-close-mobile');
 
   menuButton.addEventListener('click', function () {
-    const isMenuOpen = menu.classList.contains('block');
-    
-    if (isMenuOpen) {
+      // Toggle a visibilidade do menu
+      menu.classList.toggle('hidden');
+
+      // Trocar ícones de abrir/fechar menu
+      if (menu.classList.contains('hidden')) {
+          menuOpenIcon.classList.remove('hidden');
+          menuCloseIcon.classList.add('hidden');
+      } else {
+          menuOpenIcon.classList.add('hidden');
+          menuCloseIcon.classList.remove('hidden');
+      }
+  });
+
+  menuCloseMobile.addEventListener('click', function () {
       menu.classList.add('hidden');
-      menu.classList.remove('block');
       menuOpenIcon.classList.remove('hidden');
       menuCloseIcon.classList.add('hidden');
-    } else {
-      menu.classList.add('block');
-      menu.classList.remove('hidden');
-      menuOpenIcon.classList.add('hidden');
-      menuCloseIcon.classList.remove('hidden');
-    }
   });
 });
